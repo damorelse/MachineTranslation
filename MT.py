ca733eb9e5620b326952e14283d2a349158484f8
Gina Pai <ginapai@gmail.com>
2014-02-19 15:39:09 -0800

    actually corrects commas/periods

--- MT.py
+++ MT.py
@@ -10,11 +10,12 @@ sentences = json.load(json_data)
 engSent = []
 for line in sentences["dev"]:
 	for p in dictionary["phrases"]:
-		p = re.sub('[,.]+', '', p)
 		line.replace(" "+p+" ", " "+dictionary["phrases"][p]+" ")
+	line = re.sub('[,.]+', '', line)
 	words = line.split(" ")
 	output = []
 	for w in words:
+
 		if w in dictionary["words"]:
 			output.append(dictionary["words"][w][0])
 		elif w.lower() in dictionary["words"]:
