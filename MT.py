c3f0bc200ac2d1daf1f7526cff2c566a7c3d458f
Gina Pai <ginapai@gmail.com>
2014-02-19 15:36:42 -0800

    lowercase and actually removes commas and periods

--- MT.py
+++ MT.py
@@ -1,5 +1,6 @@
 import json
 from pprint import pprint
+import re
 
 json_data = open('dictionary.json')
 dictionary = json.load(json_data)
@@ -9,12 +10,15 @@ sentences = json.load(json_data)
 engSent = []
 for line in sentences["dev"]:
 	for p in dictionary["phrases"]:
+		p = re.sub('[,.]+', '', p)
 		line.replace(" "+p+" ", " "+dictionary["phrases"][p]+" ")
 	words = line.split(" ")
 	output = []
 	for w in words:
 		if w in dictionary["words"]:
 			output.append(dictionary["words"][w][0])
+		elif w.lower() in dictionary["words"]:
+			output.append(dictionary["words"][w.lower()][0])
 		else:
 			output.append(w)
 	engSent.append(output)
